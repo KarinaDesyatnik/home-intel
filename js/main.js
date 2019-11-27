7ecd8b6c08fa1aff0f9637ae2fcd9979541bdd9b
karinadesyatnik <karinadesyatnik27@gmail.com>
2019-11-27 22:13:16 +0300

    home intel

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+	if($(window).width() > 480) {
+   $('.slider').slick({
+	prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/left-triangle.png" alt="">',
+	nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/right-triangle.png" alt="">'
+});
+   }
+
+
+
+});
