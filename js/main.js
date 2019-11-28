04748594fae155b43a120b756334ce6eebf2c5bd
karinadesyatnik <karinadesyatnik27@gmail.com>
2019-11-28 09:29:59 +0300

    change after reviews

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 $(function(){
-
+	
 	if($(window).width() > 480) {
    $('.slider').slick({
 	prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/left-triangle.png" alt="">',
-	nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/right-triangle.png" alt="">'
+	nextArrow: $('.next')
 });
    }
-
-
 
 });
